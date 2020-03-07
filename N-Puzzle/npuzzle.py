1c46fcde87c7af38b7ce3ab3f5d1b3f681136f32
Vladyslav Fil <wladyslawfil@gmail.com>
2020-03-07 23:50:56 +0200

    Bonus needed

--- N-Puzzle/npuzzle.py
+++ N-Puzzle/npuzzle.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 
 import sys
-import os.path
+import os
+import random
+import argparse
 from puzzle import Puzzle
 
 
-def get_content():
-    if len(sys.argv) > 1:
-        fname = sys.argv[1]
-        if not os.path.isfile(fname):
-            raise Exception('*', f"Oops... File '{fname}'  doesn't exists or is a folder!")
-        with open(fname, "r") as file:
+def get_content(file_name):
+    if file_name:
+        if not os.path.isfile(file_name):
+            raise Exception('*', f"Oops... File '{file_name}' doesn't exists or is a folder!")
+        with open(file_name, "r") as file:
             return file.read()
     else:
         print("Write initial puzzle state:")
@@ -18,9 +19,23 @@ def get_content():
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "-file", help="get input from file (set file name or path). By default reads from stdin")
+    parser.add_argument("--h", "-heuristic",
+                        help="choose heuristic: mn (manhattan), eu (euclidian), mp (misplaced). Manhattan is default",
+                        choices=['mn', 'eu', 'mp'],
+                        default='mn')
+    parser.add_argument("-r", "-random", help="get random input", action="store_true")
+
+    args = parser.parse_args()
     try:
-        content = get_content()
-        puz = Puzzle(content)
+        if args.r:
+            random_file = "inputs/" + random.choice(os.listdir("inputs"))
+            print(random_file)
+            content = get_content(random_file)
+        else:
+            content = get_content(args.f)
+        puz = Puzzle(content, args.h)
         puz.solve()
         puz.print_result()
     except Exception as e:
--- N-Puzzle/puzzle.py
+++ N-Puzzle/puzzle.py
@@ -7,7 +7,7 @@ import datetime
 import math
 
 class Puzzle:
-    def __init__(self, content):
+    def __init__(self, content, h):
         self.n = 0
         self.data_len = 0
         self.opened = []
@@ -20,10 +20,15 @@ class Puzzle:
         self.goal_state = None
         self.solving_time = None
         self.line_goal = []
+        self.heuristic_dict = {
+            "eu" : self.get_euclidian_distance,
+            "mn" : self.get_manhattan_distance,
+            "mp" : self.get_misplaced_tiles_distance}
+
+        self.heuristic = self.heuristic_dict[h]
 
         heapify(self.opened)
         self.parse_content(content)
-
 
     def validate_tiles(self, tiles_line):
         res = tiles_line.split()
@@ -38,7 +43,6 @@ class Puzzle:
                 raise Exception('*', f"Tile '{tile}' is not in range 0:{self.data_len}")
             self.validated_tiles.append(tile)
         return list(map(int, res))
-
 
     def parse_content(self, content):
         content_list = list(filter(None, map(str.strip, content.strip().split("\n"))))
@@ -64,7 +68,7 @@ class Puzzle:
         if len(self.not_validated_tiles) != len(self.validated_tiles):
             raise Exception('*', "Wrong number of tiles")
         self.generate_goal_data()
-
+        self.print_start()
 
     def get_manhattan_distance(self, current):
         dist = 0
@@ -78,7 +82,6 @@ class Puzzle:
             dist += x + y
         return dist
 
-
     def get_euclidian_distance(self, current):
         dist = 0
         for i in current.oneline_data:
@@ -91,7 +94,6 @@ class Puzzle:
             dist += math.sqrt(x + y)
         return dist
 
-
     def get_misplaced_tiles_distance(self, current):
         dist = 0
         for val in current.oneline_data:
@@ -101,14 +103,11 @@ class Puzzle:
                 dist += 1
         return dist
 
-
     def f(self, current):
         return self.h(current) + current.level
 
-
     def h(self, current):
-        return self.get_manhattan_distance(current)
-
+        return self.heuristic(current)
 
     def generate_goal_data(self):
         self.goal_data = [[0] * self.n for i in range(self.n)]
@@ -148,33 +147,77 @@ class Puzzle:
             elif d == "u":
                 i -= 1
 
+    # def inversions_count(self, line_input):
+    #     #     inv = 0
+    #     #     for i in range(self.data_len - 1):
+    #     #         for j in range(i + 1, self.data_len):
+    #     #             if self.line_goal.index(line_input[i]) > self.line_goal.index(line_input[j]):
+    #     #                 inv += 1
+    #     #     return inv
+    #     #
+    #     #
+    #     # def is_solvable(self):
+    #     #     line_input = list(chain.from_iterable(self.initial_data))
+    #     #     self.line_goal = list(chain.from_iterable(self.goal_data))
+    #     #     inv_count = self.inversions_count(line_input)
+    #     #
+    #     #     check_zero_position = abs(line_input.index(0) // self.n - self.line_goal.index(0) // self.n) + abs(line_input.index(0) % self.n - self.line_goal.index(0) % self.n)
+    #     #     if check_zero_position % 2 == 0 and inv_count % 2 == 0:
+    #     #         return True
+    #     #     if check_zero_position % 2 == 1 and inv_count % 2 == 1:
+    #     #         return True
+    #     #     if self.n % 2:
+    #     #         return not inv_count % 1
+    #     #     else:
+    #     #         pos = self.line_goal.index(0) // self.n
+    #     #         if pos & 1:
+    #     #             return not inv_count % 1
+    #     #         else:
+    #     #             return inv_count % 1
 
-    def inversions_count(self, line_input):
+    def inversions_count(self, line_input, element, idx):
         inv = 0
-        for i in range(self.data_len - 1):
-            for j in range(i + 1, self.data_len):
-                if self.line_goal.index(line_input[i]) > self.line_goal.index(line_input[j]):
-                    inv += 1
+        element_row = self.line_goal.index(element) / self.n
+        element_col = self.line_goal.index(element) % self.n
+        for e in line_input[idx + 1:]:
+            if e == 0:
+                continue
+            e_row = self.line_goal.index(e) / self.n
+            e_col = self.line_goal.index(e) % self.n
+            if e_row < element_row or (e_row == element_row and e_col < element_col):
+                inv += 1
         return inv
 
     def is_solvable(self):
-        line_input = list(chain.from_iterable(self.initial_data))
         self.line_goal = list(chain.from_iterable(self.goal_data))
-        inv_count = self.inversions_count(line_input)
-        check_zero_position = abs(line_input.index(0) // self.n - self.line_goal.index(0) // self.n) + abs(line_input.index(0) % self.n - self.line_goal.index(0) % self.n)
-        if check_zero_position % 2 == 0 and inv_count % 2 == 0:
-            return True
-        if check_zero_position % 2 == 1 and inv_count % 2 == 1:
-            return True
-        if self.n % 2:
-            return not inv_count % 1
-        else:
-            pos = self.line_goal.index(0) // self.n
-            if pos & 1:
-                return not inv_count % 1
+        line_input = list(chain.from_iterable(self.initial_data))
+        zero_pos = line_input.index(0) / self.n + 1
+        inv = 0
+        for idx, val in enumerate(line_input):
+            if val == 0:
+                continue
+            inv += self.inversions_count(line_input, val, idx)
+        if self.n % 2 == 0:
+            if (self.n / 2) % 2 == 0 and (zero_pos % 2 == 1 and inv % 2 == 0) or (zero_pos % 2 == 0 and inv % 2 == 1):
+                return True
+            elif (self.n / 2) % 2 == 1 and zero_pos % 2 == inv % 2 and zero_pos % 2 != zero_pos - 1:
+                return True
             else:
-                return inv_count % 1
+                return False
+        else:
+            return inv % 2 == 0
 
+    def print_start(self):
+        print(f"Input puzzle ({self.n}x{self.n}):")
+        if self.heuristic == self.get_euclidian_distance:
+            print("Heuristic is Euclidian distance")
+        elif self.heuristic == self.get_manhattan_distance:
+            print("Heuristic is Manhattan distance")
+        elif self.heuristic == self.get_misplaced_tiles_distance:
+            print("Heuristic is Misplaced tiles")
+
+        for i in self.initial_data:
+            print(i)
 
     def print_result(self):
         path_to_goal = []
@@ -193,21 +236,18 @@ class Puzzle:
         print(f"Number of moves: {len(path_to_goal) - 1}")
         print(f"Solving time: {self.solving_time}")
 
-
     def solve(self):
         if self.is_solvable():
+            print("is solvable")
             try:
                 t1 = datetime.datetime.now()
                 initial_state = State(self.initial_data)
                 initial_state.fval = self.f(initial_state)
-
                 if initial_state.data == self.goal_data:
                     self.goal_state = initial_state
                     self.solving_time = 0
                     return
-
                 heappush(self.opened, initial_state)
-
                 self.opened_hash[initial_state.oneline_data] = initial_state
                 while len(self.opened) > 0:
                     current_state = heappop(self.opened)
@@ -234,7 +274,7 @@ class Puzzle:
             except MemoryError as e:
                 raise Exception('*', f"Oops... Can`t solve Puzzle because of memory error")
         else:
-            raise Exception('*', f"Oops... Puzzle is not solvable!")
+            raise Exception('*', f"is not solvable")
 
 
 
